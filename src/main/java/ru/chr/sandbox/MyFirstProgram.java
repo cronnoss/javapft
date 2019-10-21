1ac270731db15e0bef2e507210c0b3daa2afe01c
Evgeniy Karasev <karasevevg@gmail.com>
2019-10-21 11:09:45 +1000

    Introduce package

--- src/main/java/ru/chr/sandbox/MyFirstProgram.java
+++ src/main/java/ru/chr/sandbox/MyFirstProgram.java
@@ -1,4 +1,4 @@
-package sandbox;
+package ru.chr.sandbox;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
--- src/main/java/ru/chr/sandbox/Point.java
+++ src/main/java/ru/chr/sandbox/Point.java
@@ -1,4 +1,4 @@
-package sandbox;
+package ru.chr.sandbox;
 
 class Point {
 
