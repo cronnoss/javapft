92f3db6fe044477ac67b4ee104d64d04d32b1b17
cronnoss <karasevevg@gmail.com>
2019-10-25 16:09:19 +1000

    implement ContactModificationTests

--- src/test/java/ru/chr/addressbook/appmanager/ContactHelper.java
+++ src/test/java/ru/chr/addressbook/appmanager/ContactHelper.java
@@ -41,4 +41,12 @@ public class ContactHelper extends HelperBase {
     public void acceptAllert() {
         wd.switchTo().alert().accept();
     }
+
+    public void initContactModification() {
+        click(By.xpath("//img[@alt='Edit']"));
+    }
+
+    public void submitContactModification() {
+        click(By.xpath("(//input[@name='update'])[2]"));
+    }
 }
---
+++ src/test/java/ru/chr/addressbook/tests/ContactModificationTests.java
@@ -0,0 +1,16 @@
+package ru.chr.addressbook.tests;
+
+import org.junit.Test;
+import ru.chr.addressbook.model.ContactData;
+
+public class ContactModificationTests extends TestBase{
+
+    @Test
+    public void testContactModification() {
+        app.getNavigationHelper().gotoHome();
+        app.getContactHelper().selectContact();
+        app.getContactHelper().initContactModification();
+        app.getContactHelper().fillContactForm(new ContactData("Konstantin", "Solpakov", "44, apt 2,\\nFadeeva Str,\\nMoscow,\\n125047", "+79201211112", "ksolpakov@qq.com"));
+        app.getContactHelper().submitContactModification();
+    }
+}
