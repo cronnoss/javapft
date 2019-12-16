58e407017b46654c351999fadd786432dbc5a9e9
cronnoss <karasevevg@gmail.com>
2019-12-17 09:54:55 +1000

    implement static ApplicationManager

--- src/test/java/ru/chr/addressbook/tests/TestBase.java
+++ src/test/java/ru/chr/addressbook/tests/TestBase.java
@@ -16,7 +16,7 @@ public class TestBase {
 
     Logger logger = LoggerFactory.getLogger(TestBase.class);
 
-    protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+    protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
     @BeforeSuite
     public void setUp() {
