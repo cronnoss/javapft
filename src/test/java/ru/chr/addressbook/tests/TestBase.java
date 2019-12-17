4f5d9b9be1a7e3118d38e6669bdcf39581e5fe1f
cronnoss <karasevevg@gmail.com>
2019-12-17 11:29:21 +1000

    disabled auto import on the fly - IDEA-Settings-Editor-General-autoImport

--- src/test/java/ru/chr/addressbook/tests/TestBase.java
+++ src/test/java/ru/chr/addressbook/tests/TestBase.java
@@ -3,14 +3,19 @@ package ru.chr.addressbook.tests;
 import org.openqa.selenium.remote.BrowserType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.collections.Objects;
 import ru.chr.addressbook.appmanager.ApplicationManager;
+import ru.chr.addressbook.model.GroupData;
+import ru.chr.addressbook.model.ContactData;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class TestBase {
 
